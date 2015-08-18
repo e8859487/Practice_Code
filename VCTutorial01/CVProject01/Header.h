8a0018eadab2ef5b6848e37c3f154971aca1c65c
chris <e8859487@gmail.com>
2015-08-18 11:42:36 +0800

    刪去不必要程式碼

--- VCTutorial01/CVProject01/Header.h
+++ VCTutorial01/CVProject01/Header.h
@@ -13,21 +13,5 @@ using namespace std;
 //畫方格圖
 void tutorial00();
 void tutorial01();
-
-void tutorial02();
-void tutorial03();
-
-
-void tutorial04();
-void tutorial05();
-void tutorial06();
-void tutorial07();
-void tutorial08();
-void tutorial09();
-void tutorial10();
-void tutorial11();
-
-//外傳
-void tutorial20();
-
+ 
 #endif
